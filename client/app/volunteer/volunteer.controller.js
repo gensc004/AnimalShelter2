4fa9b1da24789bb4cf68e2cea1e44b87e390ab57
gensc004 <gensc004@morris.umn.edu>
2015-06-20 16:19:01 -0500

    Changes!

--- client/app/volunteer/volunteer.controller.js
+++ client/app/volunteer/volunteer.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('animalShelter2App')
-  .controller('VolunteerCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('VolunteerCtrl', function ($scope, $http) {
+    $scope.sections = [];
+    $scope.currentSection = {};
+    $http.get('/api/sections').success(function(awesomeThings) {
+      $scope.sections = awesomeThings;
+      $scope.currentSection = awesomeThings[0];
+    });
+    $scope.switchSection = function(section) {
+    	$scope.currentSection = section;
+
+    }
   });
