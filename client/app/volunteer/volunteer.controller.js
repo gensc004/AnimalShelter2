4ae98fd27c578ab0d9ffa374774ff44c8f63e0e5
gensc004 <gensc004@morris.umn.edu>
2015-06-20 21:06:16 -0500

    This has a lot of stuff

--- client/app/volunteer/volunteer.controller.js
+++ client/app/volunteer/volunteer.controller.js
@@ -4,6 +4,7 @@ angular.module('animalShelter2App')
   .controller('VolunteerCtrl', function ($scope, $http) {
     $scope.sections = [];
     $scope.currentSection = {};
+    $scope.openForm = true
     $http.get('/api/sections').success(function(awesomeThings) {
       $scope.sections = awesomeThings;
       $scope.currentSection = awesomeThings[0];
